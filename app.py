13d2094858dc025516e08cb2736a52a7ca11eed9
Ryan Roice <ryanroice25@gmail.com>
2025-10-10 19:49:54 +0100

    Implemented Spotify user authorization flow
    - Added /login and /callback routes to app.py
    - Added get_token() function that retrieves the access token

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask 
+from flask import Flask, redirect, request, jsonify
+import main
+import requests
+import os
 
+client_id = os.getenv("CLIENT_ID")
+redirect_uri = os.getenv("REDIRECT_URI")
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, CardioTempo!"
+    return "Hello, CardiacTempo!"
+
+@app.route("/login")
+def login():
+    scope = "user-read-playback-state user-modify-playback-state user-read-currently-playing"
+    auth_url = (
+        "https://accounts.spotify.com/authorize"
+        "?response_type=code"
+        f"&client_id={client_id}"
+        f"&scope={scope}"
+        f"&redirect_uri={redirect_uri}"
+    )
+    return redirect(auth_url)
+
+@app.route("/callback")
+def callback():
+    code = request.args.get("code")
+    token = main.get_token(code=code)
+    return jsonify({"access_token": token})
 
 if __name__ == "__main__":
-    app.run(port=8888)
+    app.run(debug=True)
---
+++ main.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+import os
+import base64
+from requests import post, get
+import json
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+redirect_uri = os.getenv("REDIRECT_URI")
+
+
+# gets token required for all future requests
+def get_token(code):
+    auth_string = client_id + ":" + client_secret
+    auth_bytes =  auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization" : "Basic " + auth_base64,
+        "Content-Type" : "application/x-www-form-urlencoded"
+    }
+    data = {
+        "code" : code,
+        "redirect_uri" : redirect_uri,
+        "grant_type" : "authorization_code"
+    }
+
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+# function that replaces headers on all future requests
+def get_auth_header(token):
+    return {"Authorization" : "Bearer" + token}
+
+
