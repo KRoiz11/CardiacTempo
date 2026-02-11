a52ed73481ec787ed3acd755f8a08d415e7515dd
Ryan Roice <ryanroice25@gmail.com>
2026-02-11 10:56:42 +0000

    Added feature to get audio features for each song in the playlist using its respective recco id

--- app.py
+++ app.py
@@ -42,15 +42,25 @@ def callback():
 
         print(playlist_length)
         recco_track_ids = []
-        for offset in range(0, 401, 40):
+        
+        for offset in range(0, playlist_length, 40):
             spotify_track_ids_set = spotify.get_playlist_items(token, playlist_id, offset)
             recco_track_ids_set = recco.get_recco_track_ids(spotify_track_ids_set)
             recco_track_ids.extend(recco_track_ids_set)
-        print(len(recco_track_ids))
+
+        for i in range(10):
+            track_id = recco_track_ids[i][0]
+            result = recco.get_track_features(track_id=track_id)
+            print(result)
+        # print(f"length:{len(recco_track_ids)}")
         return "Success"
         # return jsonify({"access_token": token}) # shows access_token on web browser for testing purposes
     except Exception as e:
         return jsonify({"Error": "token_exchange failed", "Reason": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5001,debug=True)
+
+#get features for multiple tracks at a time
+# def get_tracks_audio_features(items):
+#     for offset in range(0, len(items), 40):
--- recco.py
+++ recco.py
@@ -4,7 +4,7 @@ import json
 
 def get_recco_track_ids(track_ids):
     
-    url = f"https://api.reccobeats.com/v1/track?"
+    url = f"https://api.reccobeats.com/v1/track"
     headers = {"Accept": "application/json"}
     query = {"ids": ",".join(track_ids)}
 
@@ -12,8 +12,31 @@ def get_recco_track_ids(track_ids):
     result.raise_for_status()
     json_result = result.json()
     # print(json_result)
-    reccobeats_track_ids = [track["id"] for track in json_result["content"]]
+    reccobeats_track_ids = [(track["id"], track["trackTitle"]) for track in json_result["content"]]
     # print()
     # print(reccobeats_track_ids)
     return reccobeats_track_ids
+
+def get_track_features(track_id):
+    url = f"https://api.reccobeats.com/v1/track/{track_id}/audio-features"
+    headers = {"Accept": "application/json"}
+
+
+    result = get(url, headers=headers)
+    result.raise_for_status()
+    json_result = result.json()
+
+    return json_result
+
+def get_multiple_track_features(items):
+    url = "https://api.reccobeats.com/v1/audio-features"
+    headers = {"Accept": "application/json"}
+    query = {"ids": items}
+
+    result = get(url, headers=headers, params=query)
+    result.raise_for_status()
+    json_result = result.json()
+
+    return json_result
+
     
